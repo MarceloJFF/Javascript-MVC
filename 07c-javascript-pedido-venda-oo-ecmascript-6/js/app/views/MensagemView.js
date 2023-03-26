cb75ffe35ce337e17ba6bdacff47e5686aa4cba3
Marcelo Jhones <marceloj@acad.ifma.edu.br>
2023-03-25 21:55:47 -0300

    initial commit

---
+++ 07c-javascript-pedido-venda-oo-ecmascript-6/js/app/views/MensagemView.js
@@ -0,0 +1,13 @@
+class MensagemView extends View {
+
+    constructor(elemento ) {
+        super(elemento );
+    }
+
+    _template(mensagem ) {
+        return mensagem.texto ? 
+           `<p class="alert alert-info">${mensagem.texto}</p>` 
+           :'<p></p>';
+    }
+  
+}
---
+++ 07c-javascript-pedido-venda-oo-ecmascript-6/js/app/views/View.js
@@ -0,0 +1,17 @@
+class View {
+
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+
+    update(model ) {
+        // ...
+        this._elemento.innerHTML = this._template(model );
+        // ...
+    }
+
+    _template(model ) {
+        throw new Error('O método template deve ser implementado nas subclasses');
+    }
+
+}
